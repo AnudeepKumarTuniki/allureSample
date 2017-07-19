d7d730378a3be34b0c5124ab7df40aabc17fa81b
AnudeepKumarTuniki <anudeeptuniki@gmail.com>
2017-07-19 22:47:38 +0530

    Committing

--- src/main/java/anudeep/selenium/Sample2.java
+++ src/main/java/anudeep/selenium/Sample2.java
@@ -4,5 +4,13 @@ package main.java.anudeep.selenium;
  * Created by Anudeep on 7/19/2017.
  */
 public class Sample2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Anudeep Kumar Tuniki");
+		System.out.println("Yes  here");
+	}
+	
+	
 	
 }
